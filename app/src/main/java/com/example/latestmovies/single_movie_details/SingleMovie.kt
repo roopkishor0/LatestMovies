6ef8a333d00a149cfdff7aef42b3c24622905c72
Roop Kishore <roop.kishor1@channelplay.in>
2021-04-25 01:45:49 +0530

    uploading project on github first time

---
+++ app/src/main/java/com/example/latestmovies/single_movie_details/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.example.latestmovies.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.latestmovies.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
